c90a775dfc03d1027b16f268e20b60215d0c1a64
Lenny Kiloul <lkiloul@fu-r3-p5.s19.be>
2024-10-16 12:24:31 +0200

    Ajout des fonctions dans le libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 11:58:42 by lkiloul           #+#    #+#             */
-/*   Updated: 2024/10/16 11:38:32 by lkiloul          ###   ########.fr       */
+/*   Updated: 2024/10/16 12:23:57 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 # define LIBFT_H
 
 # include <stdio.h>
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 int		ft_isalnum(char str);
 int		ft_isalpha(char str);
@@ -34,5 +34,10 @@ int		ft_tolower(int i);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t n);
+int		ft_atoi(const char *src);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
